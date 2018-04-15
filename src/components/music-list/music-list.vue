cc84c568d02656eac16cda7ddd2e06c31ffcb271
luwei <1280380446@qq.com>
2018-04-15 15:57:38 +0800

    [ADD] 	1、完成music-list组件

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-under" ref='bgUnder'>
       <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter" ref="filter"></div>
-    </div>
+      </div>
+      <div class="filter" ref="filter">
+        <div class="play-wrapper">
+          <div class="play" ref='playBtn' v-show="songs.length">
+            <i class="icon-play"></i>
+            <span class="text">随机播放全部</span>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- <div class="bg-layer" ref="layer"></div> -->
     <scroll @scroll="scroll" :data='songs' :probeType='probeType' :listenScroll='listenScroll' class="list" ref='list'>
       <div class="song-list-wrapper">
         <song-list :songs='songs'></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -20,9 +30,13 @@
 
 <script>
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   import SongList from 'base/song-list/song-list'
+  import { prefixStyle } from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40;
+  const transform = prefixStyle('transform');
+  const filter = prefixStyle('filter');
 
   export default {
     props: {
@@ -48,18 +62,23 @@
     },
     mounted () {
       this.imageHeight = this.$refs.bgImage.clientHeight;
-      this.$refs.bgUnder.style.height = this.imageHeight;
+      this.$refs.bgUnder.style.height = this.imageHeight + 'px';
+      this.$refs.filter.style.height = this.imageHeight + 'px';
       this.minTranslateY = RESERVED_HEIGHT - this.$refs.bgImage.clientHeight;
       this.$refs.list.$el.style.top=`${this.$refs.bgImage.clientHeight}px`
     },
     methods: {
       scroll(pos) {
         this.scrollY = pos.y;
+      },
+      back() {
+        this.$router.back();
       }
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     },
     computed: {
       bgStyle() {
@@ -74,11 +93,12 @@
         const percent = Math.abs(newY / this.imageHeight)
         if(newY > 0) {
           scale = 1 + percent;
+          this.$refs.filter.style.height = this.imageHeight + translateY + 'px';
         } else {
           blur = Math.min(20*percent, 20);
           this.$refs.bgUnder.style.overflow = 'hidden';
         }
-        this.$refs.bgImage.style['filter'] = `blur(${blur}px)`;
+        this.$refs.bgImage.style[filter] = `blur(${blur}px)`;
         if (newY < this.minTranslateY) {
           this.$refs.bgImage.style.paddingTop = 0;
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
@@ -87,8 +107,8 @@
           this.$refs.bgImage.style.height = 0;
         }
         this.$refs.bgUnder.style.height = this.imageHeight + translateY + 'px';
-        this.$refs.bgImage.style['transform'] = `scale(${scale})`;
-        this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`;
+        this.$refs.bgImage.style[transform] = `scale(${scale})`;
+        this.$refs.playBtn.style[transform] = `scale(${scale})`;
       }
     },
     created() {
@@ -147,37 +167,38 @@
         transform-origin: top
         background-size: cover
         overflow: hidden
-        .play-wrapper
-          position: absolute
-          bottom: 20px
-          z-index: 50
-          width: 100%
-          .play
-            box-sizing: border-box
-            width: 135px
-            padding: 7px 0
-            margin: 0 auto
-            text-align: center
-            border: 1px solid $color-theme
-            color: $color-theme
-            border-radius: 100px
-            font-size: 0
-            .icon-play
-              display: inline-block
-              vertical-align: middle
-              margin-right: 6px
-              font-size: $font-size-medium-x
-            .text
-              display: inline-block
-              vertical-align: middle
-              font-size: $font-size-small
-        .filter
+      .filter
           position: absolute
           top: 0
           left: 0
           width: 100%
           height: 100%
           background: rgba(7, 17, 27, 0.4)
+          z-index 100
+          .play-wrapper
+            position: absolute
+            bottom: 20px
+            z-index: 50
+            width: 100%
+            .play
+              box-sizing: border-box
+              width: 135px
+              padding: 7px 0
+              margin: 0 auto
+              text-align: center
+              border: 1px solid $color-theme
+              color: $color-theme
+              border-radius: 100px
+              font-size: 0
+              .icon-play
+                display: inline-block
+                vertical-align: middle
+                margin-right: 6px
+                font-size: $font-size-medium-x
+              .text
+                display: inline-block
+                vertical-align: middle
+                font-size: $font-size-small
     .bg-layer
       position: relative
       height: 100%
